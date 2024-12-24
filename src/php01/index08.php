5b716fb9cd0b6e1d0fb7856b586c583d5a324933
y_has <yuko1640@gmail.com>
2024-12-24 11:25:52 +0900

    コミットメッセージ

---
+++ src/php01/index08.php
@@ -0,0 +1,11 @@
+<?php
+$people = array(
+  'person1' => 'Taro(25歳men)',
+  'person2'  => 'Jiro(20歳men)',
+  'person3'  => 'hanako(16歳women)'
+);
+
+foreach ($people as $person) {
+  echo $person;
+  echo '<br />';
+}
